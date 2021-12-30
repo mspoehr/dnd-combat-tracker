e810bae18bd10fb6b79c2ffd39808e787dd4ce65
Cymbre Spoehr <cymbre@mattscott.org>
2021-12-29 20:37:24 -0500

    Made creature quick add and a table to display initiative order.

---
+++ src/DisplayInitiative.tsx
@@ -0,0 +1,45 @@
+import {Button, Table} from "react-bootstrap";
+
+import { useState } from "react";
+import QuickAddModal from "./QuickAddModal"
+import { useAppSelector } from "./redux/store";
+import { selectSortedInitiativeCreatures } from "./redux/initiative-tracker/initiativeTrackerSlice";
+
+export default function DisplayInitiative() {
+
+    // const dispatch = useAppDispatch();
+    const [quickAddOpen, setQuickAddOpen] = useState(false);    
+    const handleClose = () => setQuickAddOpen(false);   
+    const initiativeCreatures = useAppSelector(selectSortedInitiativeCreatures);
+
+    return (
+    <div>
+        <Button variant="outline-primary" size="lg" onClick={() => setQuickAddOpen(true) }>Quick Add Character</Button>
+        <QuickAddModal open={quickAddOpen} close={handleClose} />
+        {}
+
+        <Table striped bordered hover>
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Name</th>
+                    <th>AC</th>
+                    <th>Max HP</th>
+                    <th>Initiative</th>
+                    <th>Quantity</th>
+                </tr>
+            </thead>
+            <tbody>
+                {initiativeCreatures.map((creature,index) => <tr key={index}>
+                    <td>{index + 1}</td>
+                    <td>{creature.name}</td>
+                    <td>{creature.ac}</td>
+                    <td>{creature.maxHp}</td>
+                    <td>{creature.initiative}</td>
+                    <td>0</td>
+                </tr>)}                
+            </tbody>
+        </Table>
+    </div>
+  )
+}
